8993dfade71ab895fe466e51821b13b7355ec9c8
Mkelman18 <160253161+Mkelman18@users.noreply.github.com>
2025-04-04 11:32:05 -0300

    Add files via upload

---
+++ Exemplo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exemplo {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.n);
+
+        sc.close();
+        
+    }
+    
+
+    
+}
